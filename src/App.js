b04e86a148ef4e83b8fc7650ac5183bfd7e3d8eb
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-02-21 03:25:37 +0530

    auto download

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './kide_logo.svg';
+import loader from './loader.svg';
 import './App.css';
 
 function App() {
+  useEffect(()=> {
+    document.getElementById("download").click();
+  })
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +14,19 @@ function App() {
         <p>
           Kide - The Ultimate KIIT Experience!
         </p>
+        <div className="subtext">
+          Please wait, the download has started...
+        </div>
+        <img src={loader} className="loader" width = "30px" alt="logo" />
+        <div className="subtext">
+          If download doesn't start in a while, click on the button below.
+        </div>
         <button
           className="App-link"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <a href="https://www.dropbox.com/s/kdmsmya4e5dd57l/app-release.apk?dl=1" className="link">
+          <a id="download" href="https://www.dropbox.com/s/kdmsmya4e5dd57l/app-release.apk?dl=1" className="link">
             Download Now
           </a>
         </button>
